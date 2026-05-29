cee7913c6aae9cdb3b56150ef7f4d9dba367241d
unknown <vahidgh2493@gmail.com>
2026-05-29 21:36:55 +0330

    scanner page added

---
+++ Scanner.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>QR Scanner</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <h2>QR Attendance Scanner</h2>
+
+    <div id="reader"></div>
+
+    <div id="result"></div>
+
+    <script src="js/html5-qrcode.min.js"></script>
+</body>
+
+</html>
